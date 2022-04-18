b7e47820eef78a212fbe89492fbf92754cafaa7a
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2022-04-18 19:27:39 +0200

    cimlap hotfix
    'oldal nem található' fixed when searching for the opening page

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ include('./includes/config.inc.php');
 
 //$keres = current($oldalak);
 $keres = $oldalak['/'];
-if(!isset($_SESSION['login'])){
-	$keres = $hiba_oldal;
-	header("HTTP/1.0 404 Not Found");
-}
 if (isset($_GET['oldal'])) {
 	if (isset($oldalak[$_GET['oldal']]) && file_exists("./templates/pages/{$oldalak[$_GET['oldal']]['fajl']}.tpl.php")) {
 		$keres = $oldalak[$_GET['oldal']];
